07eee3254ada3cb1860763648d55445db4c64d2e
ahmedtarek- <ahmed.t.abdelfattah@gmail.com>
2017-01-29 19:00:33 +0200

    Added some hyperlinks and about view

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse("<h1>Hello there captain Hohez</h1>")
+	return HttpResponse("<h1>Hello there captain Hohez\n</h1> <a href='/rango/about'> About</a>")
+
+
+def about(request):
+	return HttpResponse("<h1> ALL ABOUT US, THE HOHEZ </h2> <a href='/rango'> Home</a>")
